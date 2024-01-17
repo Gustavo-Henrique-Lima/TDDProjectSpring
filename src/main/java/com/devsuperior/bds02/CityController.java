ce8672975ea049380f3188e69eaba372b86446f2
Gustavo Henrique <gustavohgustavo@hotmail.com>
2024-01-17 10:29:46 -0300

    feature: Solved findAllShouldReturnAllResourcesSortedByName test.

---
+++ src/main/java/com/devsuperior/bds02/CityController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.bds02;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.services.CityService;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+	@Autowired
+	private CityService service;
+
+	@GetMapping
+	public ResponseEntity<List<CityDTO>> findAllCitySorte() {
+		List<CityDTO> cityDTO = service.findAllSorted();
+		return ResponseEntity.ok(cityDTO);
+	}
+}
